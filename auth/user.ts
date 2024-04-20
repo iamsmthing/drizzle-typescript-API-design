df91244e70deabfb2fff1d1a25728807d286d886
roy-shivam164 <shivam.rai@cginfinity.com>
2024-04-20 16:07:35 +0530

    added functionality of set-cookie while login and signup

--- auth/user.ts
+++ auth/user.ts
@@ -4,6 +4,7 @@ import { asc,eq } from 'drizzle-orm';
 import { v4 as uuidv4 } from 'uuid';
 import {Request, Response } from 'express';
 import { comparePassword, generateToken, hashPassword } from './auth';
+import { serialize } from "cookie";
 
 export const createNewUser=async(req:Request,res:Response)=>{
     const checkIfUserExists = await db.select().from(users).where(eq(users.email, req.body.email));
@@ -18,9 +19,20 @@ export const createNewUser=async(req:Request,res:Response)=>{
         const user =await db.insert(users).values({id:uuidv4(),name:req.body.name,email:req.body.email,password:hash}).returning();
 
     console.log(user)
-    const token=generateToken({id:user[0].id})
-    console.log("token:",token)
-    res.json({token:token})
+    if(user){
+
+        const token=generateToken({id:user[0].id});
+        res.setHeader(
+            "Set-Cookie",
+            serialize(process.env.JWT_SECRET!, token, {
+              httpOnly: true,
+              path: "/",
+              maxAge: 60 * 60 * 24 * 7,
+            })
+        );
+        console.log("token:",token)
+        res.json({token:token})
+    }
         
     } catch (error) {
         console.log(error)
@@ -41,8 +53,21 @@ export const signIn=async(req:Request,res:Response)=>{
         if (!isPasswordValid) {
             return res.status(401).json({ error: 'Invalid password' });
         }
-        const token=generateToken({id:user[0].id})
-        return res.json({token:token})
+
+        if(isPasswordValid){
+
+            const token=generateToken({id:user[0].id})
+            res.setHeader(
+                "Set-Cookie",
+                serialize(process.env.JWT_SECRET!, token, {
+                  httpOnly: true,
+                  path: "/",
+                  maxAge: 60 * 60 * 24 * 7,
+                })
+              );
+              return res.json({token:token})
+        }
+        
         
     } catch (error) {
         res.json(error)
